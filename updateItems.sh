05d58ec4cacc3f7cd6b441811ef8bda24b2687e5
jiggersplat <dan@Dans-Mac.local>
2023-09-23 12:42:10 -0400

    update locations

--- updateItems.sh
+++ updateItems.sh
@@ -3,6 +3,7 @@
 if ! diff -q Items.data ~/Library/Caches/com.apple.findmy.fmipcore/Items.data 
 &>/dev/null; then
 	cp ~/Library/Caches/com.apple.findmy.fmipcore/Items.data .
+	git add -a
 	git commit -m "update locations"
 	git push
 fi
