63266c2fc546b24dd8337ffcd3f7e120081bc54b
jiggersplat <jiggersplat@hotmail.com>
2023-09-23 19:26:43 -0400

    Update updateItems.sh

--- updateItems.sh
+++ updateItems.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+cd "$(dirname "$0")"; 
 if ! diff -q Items.data ~/Library/Caches/com.apple.findmy.fmipcore/Items.data 
 &>/dev/null; then
 	cp ~/Library/Caches/com.apple.findmy.fmipcore/Items.data .
